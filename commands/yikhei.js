06b23188bf58dc17abfc5028ebf9c8a99dfffcc8
JorgenJorgensenSonOfJorgen <55201515+JorgenJorgensenSonOfJorgen@users.noreply.github.com>
2021-01-26 09:55:39 -0700

    Update yikhei.js

--- commands/yikhei.js
+++ commands/yikhei.js
@@ -2,6 +2,6 @@ module.exports = {
     name : "yikhei",
     description: "this is a ping command",
     execute(message,args){
-        message.channel.send('36, 3, 24');
+        message.channel.send('jo mam');
     }
 }
